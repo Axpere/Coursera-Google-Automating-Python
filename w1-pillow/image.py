0c2e9d2713e6ce22ab69289fbf575815b1899413
Axpere <11291858+Axpere@users.noreply.github.com>
2021-01-19 16:32:44 +0100

    added comments

--- w1-pillow/image.py
+++ w1-pillow/image.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
-import os, sys
-from PIL import Image
+import os, sys #imports os and sys for directory operations
+from PIL import Image #imports Image function from PIL or pillow library for image edition
 
 #Defining output options
-size = (128, 128)
-rotation = 270
-end_dirc = "/opt/icons/"
+size = (128, 128) #pixel width and heigth
+rotation = 270 #90 deg clockwise
+end_dirc = "/opt/icons/" #destination
 
+#obtain file names in the origin directory
 for root, dirs, files in os.walk("/home/student-02-fd4579322602/images/"):
-  for file in files:
-   final_dirc = "/opt/icons/" + file
+  for file in files: #iterate for each file (name)
+   final_dirc = "/opt/icons/" + file #create final destination with directory + name
    try:
-      with Image.open(file) as pic:
+      with Image.open(file) as pic: #opens file as object
+        #edits the image object, the RGB conversion is a necesary step in this case
         pic.rotate(rotation).resize(size).convert("RGB").save(final_dirc, "JPEG")
    except OSError:
       print("Cannot convert", file)
